b624499be3067aff575381ae7047980729ec7bda
steph-wright <wright.steph.00@gmail.com>
2023-01-03 14:01:20 +0000

    add skill

---
+++ src/main/java/com/steph/controller/SkillController.java
@@ -0,0 +1,61 @@
+package com.steph.controller;
+
+import com.steph.entity.Skill;
+import com.steph.entity.SkillId;
+import com.steph.entity.UserDetails;
+import com.steph.model.service.SkillService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+public class SkillController {
+
+    @Autowired
+    SkillService skillService;
+
+    @RequestMapping("/manageSkills")
+    public ModelAndView manageSkillsController(HttpSession session) {
+        ModelAndView mv = new ModelAndView();
+
+        UserDetails user = (UserDetails) session.getAttribute("user");
+        List<Skill> skills = skillService.findByUsername(user.getUsername());
+
+        mv.addObject("skills", skills);
+        mv.setViewName("manageSkills");
+        return mv;
+    }
+
+    @RequestMapping("/addSkill")
+    public ModelAndView addSkillController() {
+        return new ModelAndView("addSkill");
+    }
+
+    @RequestMapping("/addedSkill")
+    public ModelAndView addedSkillController(HttpSession session, @RequestParam("skillName") String skillName, @RequestParam("explanation") String explanation) {
+        ModelAndView mv = new ModelAndView();
+        UserDetails user = (UserDetails) session.getAttribute("user");
+
+        SkillId skillId = new SkillId(user.getUsername(), skillName);
+        Skill newSkill = new Skill(skillId, true, explanation);
+
+        Skill addedSkill = skillService.createSkill(newSkill);
+        String message = null;
+
+        if (addedSkill == null) {
+            message = "A skill with that name already exists.";
+        } else {
+            message = "Skill successfully added.";
+        }
+
+        mv.setViewName("addSkill");
+        mv.addObject("message", message);
+        return mv;
+    }
+
+}
